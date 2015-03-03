38434c36cd28f16e7f51eae1eaa0bfea7ff3c59b
bagustris <bagustris@yahoo.com>
2015-03-03 19:03:51 +0700

    revisi bintang spasi mundur

--- bintangSpasiMundur.m
+++ bintangSpasiMundur.m
@@ -4,13 +4,18 @@ clear all; close all;
 
 n=input('Masukkan jumlah baris: ');
 
-for i = 1:n 
-    
+for i = 1:n 	    
+	
+	% cetak spasi    
+	for j = 1:i-1
+    fprintf(' ');
+    end
+
     % cetak bintang
     for j = 1:n
     fprintf('*');
     end
-    
+
     fprintf('\n');
     n = n - 1;
      
