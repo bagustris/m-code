bd0f18cdb87302364f58b537cb073b776c7a8f82
bagustris <bagustris@linuxmail.org>
2015-04-07 17:00:25 +0700

    modified:   uts2015c.m

--- uts2015c.m
+++ uts2015c.m
@@ -6,14 +6,14 @@ disp('=======================');
 a = input('Masukkan Bilangan Pertama : ');
 n = input('Masukkan Banyak bilangan : ');
 disp('=======================');
-disp('Deret Bilangan adalah : ');
+fprintf('Deret Bilangan adalah : ');
 jumlah = 0;
 
 for i=1:n,
     r=(i+1)^2;
-    disp([num2str(a)]);
+    fprintf([num2str(a) ' ']);
     jumlah=jumlah+a;
-    a=a+r;
+    a=a+r; 
 end
-
+fprintf('\n');
 disp(['Jumlah deret Bilangan: ' num2str(jumlah)]);
